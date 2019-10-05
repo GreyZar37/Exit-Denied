98bddba8682122390a1cf5899a7e9b2034fc8ca3
GreyZar37 <52458016+GreyZar37@users.noreply.github.com>
2019-10-05 20:20:17 +0200

    DH

---
+++ (Exit.Denied);/Assets/hamster.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hamster : MonoBehaviour
+{
+
+    public float Speed = 100;
+    public Rigidbody rb;
+
+    public float RotationSpeed = 100;
+
+ 
+
+ 
+    void Update()
+    {
+
+        Vector3 movement = new Vector3();
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            movement += transform.forward; 
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -RotationSpeed*Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, RotationSpeed * Time.deltaTime, 0);
+        }
+
+        rb.velocity = movement*Speed* Time.deltaTime;
+
+
+    }
+}
