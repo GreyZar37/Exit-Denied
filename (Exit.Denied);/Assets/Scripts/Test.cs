9b0561bcd910836898123e73b65f4b6d9addfa77
Nicolatian <54408811+Nicolatian@users.noreply.github.com>
2019-10-06 11:31:57 +0200

    frtghdfyd

---
+++ (Exit.Denied);/Assets/Scripts/Test.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    [Header("Variables")]
+    public float activatedOffset = 0.1f;
+    public LayerMask layerMask;
+    public string objectTag;
+
+    [Header("Values -- DO NOT ALTER")]
+    public bool activated = false;
+
+    void Start()
+    {
+        Debug.Log("SCRIPT STARTED.");
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.layer);
+        if (collision.gameObject.CompareTag(objectTag))
+        {
+            activated = true;
+            transform.position = new Vector3(transform.position.x, transform.position.y - activatedOffset, transform.position.z);
+            Debug.Log("Activated.");
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Collision exit.");
+        if (collision.gameObject.CompareTag(objectTag))
+        {
+            activated = false;
+            transform.position = new Vector3(transform.position.x, transform.position.y + activatedOffset, transform.position.z);
+            Debug.Log("De-activated.");
+        }
+    }
+
+}
